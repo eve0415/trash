a909f7bf3e4285f3fc7ab6fb882433730d0ed62c
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-12-01 00:12:31 +0900

    Third commit

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,55 @@ const initEvent = (client: Client) => {
         cmd.run(message, args);
     });
 
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        if (!oldState.channel && newState.channel) {
+            console.log('join');
+            client.emit('voiceChannelJoin', newState);
+        } else if (oldState.channel && !newState.channel) {
+            console.log('leave');
+            client.emit('voiceChannelLeave', newState);
+        } else if (!oldState.selfDeaf && newState.selfDeaf) {
+            console.log('deaf');
+            client.emit('voiceDeaf', newState);
+        } else if (oldState.selfDeaf && !newState.selfDeaf) {
+            console.log('undeaf');
+            client.emit('voiceUndeaf', newState);
+        } else if (!oldState.selfMute && newState.selfMute) {
+            console.log('mute');
+            client.emit('voiceMute', newState);
+        } else if (oldState.selfMute && !newState.selfMute) {
+            console.log('unmute');
+            client.emit('voiceUnmute', newState);
+        } else if (!oldState.serverDeaf && newState.serverDeaf) {
+            console.log('deaf by server');
+            client.emit('voiceServerwideDeaf', newState);
+        } else if (oldState.serverDeaf && !newState.serverDeaf) {
+            console.log('deaf by server');
+            client.emit('voiceServerwideUndeaf', newState);
+        } else if (!oldState.serverMute && newState.serverMute) {
+            console.log('mute by server');
+            client.emit('voiceServerwideMute', newState);
+        } else if (oldState.serverMute && !newState.serverMute) {
+            console.log('unmute by server');
+            client.emit('voiceServerwideUnmute', newState);
+        } else if (!oldState.selfVideo && newState.selfVideo) {
+            console.log('start video');
+            client.emit('voiceVideoStart', newState);
+        } else if (oldState.selfVideo && !newState.selfVideo) {
+            console.log('stop video');
+            client.emit('voiceVideoStop', newState);
+        } else if (!oldState.streaming && newState.streaming) {
+            console.log('stream start');
+            client.emit('voiceStreamStart', newState);
+        } else if (oldState.streaming && !newState.streaming) {
+            console.log('stream stop');
+            client.emit('voiceStreamStop', newState);
+        } else if (oldState.channel && newState.channel) {
+            console.log('change');
+            client.emit('voiceChannelChange', oldState, newState);
+        }
+    });
+
     client.login().catch(console.error);
 };
 
